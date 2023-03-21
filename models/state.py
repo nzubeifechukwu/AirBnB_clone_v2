9a0eff426891052238ba82b3817d8b188fca09ae
Martins Ndifon <martinsndifon@gmail.com>
2023-03-21 11:11:35 +0000

    Fixed merging issues and update some files

--- models/state.py
+++ models/state.py
@@ -3,9 +3,10 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
@@ -14,4 +15,9 @@ class State(BaseModel):
     @property
     def cities(self):
         """Gets the cities attribute"""
-        return self.__cities
+        dict_city = storage.all(City)
+        ouput = []
+        for city in dict_city:
+            if self.id == city.state_id:
+                output.append(city)
+        return output
