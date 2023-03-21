0d131e99145ab00162a7ad670b8736e3b0c65e94
Martins Ndifon <martinsndifon@gmail.com>
2023-03-21 21:55:17 +0000

    Made some changes up to task 8

--- models/place.py
+++ models/place.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
-class Place(BaseModel):
-    """ A place to stay """
+class Place(BaseModel, Base):
+    """Represents the Place table in the db"""
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'))
     user_id = Column(String(60), ForeignKey('users.id'))
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), default='None')
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
-    amenity_ids = []
+    latitude = Column(Float)
+    longitude = Column(Float)
