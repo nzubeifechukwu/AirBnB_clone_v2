06432eb70ba72b154ba8a4471d0802c74ab84692
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-04-09 00:45:29 -0700

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,10 +5,50 @@
 from fabric.api import env, local, put, run
 from datetime import datetime
 import os.path
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 env.hosts = ['34.204.82.149', '107.23.209.253']
+
+
+def do_pack():
+    '''Creates a .tgz archive from the contents of the folder `web_static`
+
+    Return:
+        Path to the archive if successful. None otherwise
+    '''
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    file_path = 'versions/web_static_{}.tgz'.format(now)
+
+    local('mkdir -p versions')
+    local('tar -cvzf {} web_static'.format(file_path))
+
+    if os.path.exists(file_path):
+        return file_path
+    return None
+
+
+def do_deploy(archive_path):
+    '''Distribute an archive to web servers
+
+    Args:
+        archive_path: Path to a .tgz archive
+
+    Return:
+        True if all operations succeed. False otherwise
+    '''
+    try:
+        file_no_ext = archive_path.split('/')[1].split('.')[0]
+        uncomp_folder = '/data/web_static/releases/{}'.format(file_no_ext)
+        put(local_path=archive_path, remote_path='/tmp/')
+        run('mkdir -p {}'.format(uncomp_folder))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(file_no_ext, uncomp_folder))
+        run('rm /tmp/{}.tgz'.format(file_no_ext))
+        run('mv {}/web_static/* {}'.format(uncomp_folder, uncomp_folder))
+        run('rm -rf {}/web_static'.format(uncomp_folder))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(uncomp_folder))
+        return True
+    except Exception as e:
+        return False
 
 
 def deploy():
